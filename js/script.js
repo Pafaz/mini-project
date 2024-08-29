3dd8668302e83b86b76154ae79e91fed88ee066a
Pafaz <paundra.afif@gmail.com>
2024-08-29 20:43:27 +0700

    final

--- js/script.js
+++ js/script.js
@@ -94,6 +94,34 @@ myForm.addEventListener("submit", (event) => {
     }
 })
 
-ScrollReveal().reveal('.facdiv');
 
-particlesJS("hero", { "particles": { "number": { "value": 80, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#000000" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#000000", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true }); var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function () { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+let indexSlide = 1;
+showBanner(indexSlide); // Tampilkan slide pertama
+
+function nextSlide(n) {
+    showBanner(indexSlide += n);
+}
+
+function showBanner(n) {
+    const banners = document.getElementsByClassName("banner-image");
+
+    if (n > banners.length) {
+        indexSlide = 1; // Jika mencapai akhir, kembali ke slide pertama
+    }
+
+    let index = 0;
+    while (index < banners.length) {
+        banners[index].style.display = "none"; // Tampilkan semua slide
+        index++;
+        console.log(index);
+    }
+
+    // Tampilkan slide yang dituju
+    banners[indexSlide - 1 ].style.display = "block"; // Tampilkan slide saat ini
+}
+
+// Setiap 3 detik, slide berpindah
+setInterval(function() {
+    nextSlide(1); 
+}, 3000); 
+
